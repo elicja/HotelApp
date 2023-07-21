bb49df44ca7d5ff3806dcd8407a3579e40c990b8
Alicja Kowalska <niemogeznalezclaptopa@gmail.com>
2023-07-21 13:38:26 +0200

    Creating a guestbook

--- HotelAppLibrary/Data/SqlData.cs
+++ HotelAppLibrary/Data/SqlData.cs
@@ -20,4 +20,40 @@ public class SqlData
                                              connectionStringName,
                                              true);
     }
+
+    public void BookGuest(string firstName,
+                          string lastName,
+                          DateTime startDate,
+                          DateTime endDate,
+                          int roomTypeId)
+    {
+        GuestModel guest = _db.LoadData<GuestModel, dynamic>("dbo.spGuests_Insert",
+                                                             new { firstName, lastName },
+                                                             connectionStringName,
+                                                             true).First();
+
+        RoomTypeModel roomType = _db.LoadData<RoomTypeModel, dynamic>("select * from dbo.RoomTypes where Id = @Id",
+                                                                      new { Id = roomTypeId },
+                                                                      connectionStringName,
+                                                                      false).First();
+
+        TimeSpan timeStaying = endDate.Date.Subtract(startDate.Date);
+
+        List<RoomModel> availableRooms = _db.LoadData<RoomModel, dynamic>("dbo.spRooms_GetAvailableRooms",
+                                                                          new { startDate, endDate, roomTypeId },
+                                                                          connectionStringName,
+                                                                          true);
+
+        _db.SaveData("dbo.spBookings_Insert",
+                     new 
+                     { 
+                         roomId = availableRooms.First().Id, 
+                         guestId = guest.Id,
+                         startDate = startDate,
+                         endDate = endDate,
+                         totalCost = timeStaying.Days * roomType.Price
+                     },
+                     connectionStringName,
+                     true);
+    }
 }
---
+++ HotelAppLibrary/Models/RoomModel.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelAppLibrary.Models;
+
+public class RoomModel
+{
+    public int Id { get; set; }
+    public string RoomNumber { get; set; }
+    public int RoomTypeId { get; set; }
+}
