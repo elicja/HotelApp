c4d8d169a8cc6f2136f1d4e2cdded66ab4e9ae1c
Alicja Kowalska <niemogeznalezclaptopa@gmail.com>
2023-07-26 12:21:34 +0200

    Filling queries for Book Guest in SQLite

--- HotelAppLibrary/Data/SqliteData.cs
+++ HotelAppLibrary/Data/SqliteData.cs
@@ -20,7 +20,60 @@ public class SqliteData : IDatabaseData
 
     public void BookGuest(string firstName, string lastName, DateTime startDate, DateTime endDate, int roomTypeId)
     {
-        throw new NotImplementedException();
+        string sql = @"select 1 from Guests where FirstName = @firstName and LastName = @lastName";
+        int results = _db.LoadData<dynamic, dynamic>(sql, new { firstName, lastName }, connectionStringName).Count();
+
+        if (results == 0)
+        {
+            sql = @"insert into Guests (FirstName, LastName)
+		                    values (@firstName, @lastName);";
+
+            _db.SaveData(sql, new { firstName, lastName }, connectionStringName);
+        }
+
+        sql = @"select [Id], [FirstName], [LastName]
+	                    from Guests
+	                    where FirstName = @firstName and LastName = @lastName LIMIT 1;";
+
+        GuestModel guest = _db.LoadData<GuestModel, dynamic>(sql,
+                                                             new { firstName, lastName },
+                                                             connectionStringName).First();
+
+        RoomTypeModel roomType = _db.LoadData<RoomTypeModel, dynamic>("select * from RoomTypes where Id = @Id",
+                                                                      new { Id = roomTypeId },
+                                                                      connectionStringName).First();
+
+        TimeSpan timeStaying = endDate.Date.Subtract(startDate.Date);
+
+        sql = @"select r.*
+	            from Rooms r
+	            inner join RoomTypes t on t.Id = r.RoomTypeId
+	            where r.RoomTypeId = @roomTypeId
+	            and r.Id not in (
+	            select b.RoomId
+	            from Bookings b
+	            where (@startDate < b.StartDate and @endDate > b.EndDate)
+		            or (b.StartDate <= @endDate and @endDate < b.EndDate)
+		            or (b.StartDate <= @startDate and @startDate < b.EndDate)
+	            );";
+
+        List<RoomModel> availableRooms = _db.LoadData<RoomModel, dynamic>(sql,
+                                                                          new { startDate, endDate, roomTypeId },
+                                                                          connectionStringName);
+
+        sql = @"insert into Bookings(RoomId, GuestId, StartDate, EndDate, TotalCost)
+	            values (@roomId, @guestId, @startDate, @endDate, @totalCost);";
+
+        _db.SaveData(sql,
+                     new
+                     {
+                         roomId = availableRooms.First().Id,
+                         guestId = guest.Id,
+                         startDate = startDate,
+                         endDate = endDate,
+                         totalCost = timeStaying.Days * roomType.Price
+                     },
+                     connectionStringName);
     }
 
     public void CheckInGuest(int bookingId)
@@ -53,7 +106,13 @@ public class SqliteData : IDatabaseData
 
     public RoomTypeModel GetRoomTypeById(int id)
     {
-        throw new NotImplementedException();
+        string sql = @"select [Id], [Title], [Description], [Price]
+	                    from RoomTypes
+	                    where Id = @id;";
+
+        return _db.LoadData<RoomTypeModel, dynamic>(sql,
+                                                    new { id },
+                                                    connectionStringName).FirstOrDefault();
     }
 
     public List<BookingFullModel> SearchBookings(string lastName)
